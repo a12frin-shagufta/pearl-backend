83b2abc8887881ba9e9a88166e6aad66eb6077e2
shagufta Afrin <shagufta7572@gmail.com>
2025-07-18 14:56:38 +0530

    improve product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,10 +3,10 @@ import multer from "multer";
 import { addProduct } from "../controller/productController.js";
 import verifyAdminToken from "../middleware/verifyAdminToken.js";
 
-const router = express.Router();
+const productRouter = express.Router();
 const storage = multer.diskStorage({});
 const upload = multer({ storage });
 
-router.post("/add", verifyAdminToken, upload.any(), addProduct);
+productRouter.post("/add", verifyAdminToken, upload.any(), addProduct);
 
 export default router;
