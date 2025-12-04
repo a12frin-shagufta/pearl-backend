364b237c1822aa8ca9444ba9152763eb431d889f
shagufta Afrin <shagufta7572@gmail.com>
2025-12-04 12:56:44 +0530

    d

--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,5 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
 import productModel from "../models/productModel.js";
 import fs from "fs";
 import mongoose from "mongoose";
